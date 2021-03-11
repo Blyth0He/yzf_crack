b405cfc9e08347759030feb084f34cf6b2c5527f
blyth <root@blyth.top>
2021-03-11 15:04:22 +0800

    first commit

---
+++ yzf_crack.py
@@ -0,0 +1,39 @@
+from base64 import b64decode
+from Crypto.Hash import MD5
+from Crypto.Cipher import AES
+import sys
+
+
+def build_passphase(base64_str):
+    base64d_str = b64decode(base64_str).decode()
+    if '-' in base64d_str:
+        params = base64d_str.split('-')
+        num = int(params[0]) - int(params[1])
+    else:
+        num = int(base64d_str)
+    key = int(num) ** 2 % 4877
+    md5hash = MD5.new()
+    md5hash.update(str(key).encode())
+    passphase = md5hash.hexdigest()[0x8:0x18]
+    return passphase
+
+class CryptoJs:
+    def __init__(self, key:bytes) -> None:
+        self.aes = AES.new(key, AES.MODE_ECB)
+
+    def _strip_padding(self, padded_bytes:bytes):
+        return padded_bytes.strip(padded_bytes[-1].to_bytes(1,'little'))
+
+    def decrypt(self, b64_cipher_text: str) -> bytes:
+        plain_bytes = self.aes.decrypt(b64decode(b64_cipher_text))
+        return self._strip_padding(plain_bytes)
+
+if '__main__' == __name__:
+    if len(sys.argv) !=3:
+        exit('USAGE: py ' + sys.argv[0] + ' [key] [encrypted_text]')
+    key = sys.argv[1]
+    encrypted = sys.argv[2]
+
+    passphrase = build_passphase(key)
+    cj = CryptoJs(passphrase.encode())
+    print(cj.decrypt(encrypted).decode())
